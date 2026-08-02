bb75307ab96bf88c640346346506e5bc4cf0c0dc
MK-986123 <58072309+MK-986123@users.noreply.github.com>
2026-08-02 18:46:46 +0000

    refactor: extract cv image conversions to util functions

--- invokeai/app/invocations/cv.py
+++ invokeai/app/invocations/cv.py
@@ -3,12 +3,13 @@
 
 import cv2 as cv
 import numpy
-from PIL import Image, ImageOps
+from PIL import ImageOps
 
 from invokeai.app.invocations.baseinvocation import BaseInvocation, invocation
 from invokeai.app.invocations.fields import ImageField, InputField, WithBoard, WithMetadata
 from invokeai.app.invocations.primitives import ImageOutput
 from invokeai.app.services.shared.invocation_context import InvocationContext
+from invokeai.backend.image_util.util import cv2_to_pil, pil_to_cv2
 
 
 @invocation("cv_inpaint", title="OpenCV Inpaint", tags=["opencv", "inpaint"], category="inpaint", version="1.3.1")
@@ -23,16 +24,14 @@ class CvInpaintInvocation(BaseInvocation, WithMetadata, WithBoard):
         mask = context.images.get_pil(self.mask.image_name)
 
         # Convert to cv image/mask
-        # TODO: consider making these utility functions
-        cv_image = cv.cvtColor(numpy.array(image.convert("RGB")), cv.COLOR_RGB2BGR)
+        cv_image = pil_to_cv2(image)
         cv_mask = numpy.array(ImageOps.invert(mask.convert("L")))
 
         # Inpaint
         cv_inpainted = cv.inpaint(cv_image, cv_mask, 3, cv.INPAINT_TELEA)
 
         # Convert back to Pillow
-        # TODO: consider making a utility function
-        image_inpainted = Image.fromarray(cv.cvtColor(cv_inpainted, cv.COLOR_BGR2RGB))
+        image_inpainted = cv2_to_pil(cv_inpainted)
 
         image_dto = context.images.save(image=image_inpainted)
 
